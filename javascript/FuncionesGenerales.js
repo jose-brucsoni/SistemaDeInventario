c23f8a8aa34cf14fb2d4c0967b3fe26985309dbf
jose_suarez <joseca6520@gmail.com>
2023-11-03 15:45:44 -0400

    Ventana inicio al 80 por ciento y la de inventario esta al 40%

--- javascript/FuncionesGenerales.js
+++ javascript/FuncionesGenerales.js
@@ -2,6 +2,7 @@
 const Select_Tema = document.getElementById('Temas_Selector');
 const Link_Estilos_Generales = document.getElementById('Link_Estilos_Generales');
 
+var servidorActual = window.location.origin + '';
 
 // Selector de Temas Posterior Cambiar de temas al ser seleccionado
 Select_Tema.addEventListener('change', function(event) {
@@ -10,29 +11,39 @@ Select_Tema.addEventListener('change', function(event) {
     // Obtener el valor seleccionado en el select
     const valor = Select_Tema.value;
     // Concatenar el valor con el enlace de estilos
-    const hrefNuevo = "./css/bootstrap-" + valor + ".css";
+    const hrefNuevo = "/css/bootstrap-" + valor + ".css";
     // Cambiar el atributo href del enlace de estilos
     Link_Estilos_Generales.href = hrefNuevo;
     
     //CambiarFondoDeLosGraficos();
     
 });
-/*
-function CambiarFondoDeLosGraficos(){
-    // Obtén el color de fondo del Body
-    setTimeout(function () {
-        //Para Cambiar el color de fondo de los graficos
-        const body = document.getElementById('body');
-        const ColorDeFondo_Del_Body = getComputedStyle(document.body).backgroundColor;
-        console.log('El color de fondo del body es:', ColorDeFondo_Del_Body);
-        // Busca los elementos rect con la clase .piechart
-        const rectPieChart = document.querySelector('#piechart div div div svg rect');
-        console.log(rectPieChart);
-        rectPieChart.style.fill = ColorDeFondo_Del_Body;
-      }, 50);
-    
-}*/
 
 function recargarPagina(){
     location.reload();
 }
+
+//Buscar en cookie si ha seleccionado un tema previamente
+
+window.addEventListener("load", function() {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i].trim();
+      if (cookie.indexOf("tema=") === 0) {
+        var temaAlmacenado = cookie.substring(5); // Obtén el valor de la cookie
+        const hrefNuevo = servidorActual+'/css/bootstrap-'+ temaAlmacenado + ".css";
+        Link_Estilos_Generales.href = hrefNuevo;
+        
+        /*
+        if (temaActual !== temaAlmacenado) {
+          document.getElementById(temaActual).disabled = true;
+          document.getElementById(temaAlmacenado).disabled = false;
+        }*/
+        break; // Detén el bucle una vez que se encuentra la cookie
+      }else{
+        const hrefNuevo = servidorActual+'/css/bootstrap-lux.css';
+        Link_Estilos_Generales.href = hrefNuevo;
+    
+      }
+    }
+  });
---
+++ javascript/funcionesInventario.js
@@ -0,0 +1,5 @@
+function Click_En_Tab(tab_seleccionado){
+    var id_del_tab = 'tab_' + tab_seleccionado;
+    const tab = document.getElementById(id_del_tab);
+    tab.click()
+}
